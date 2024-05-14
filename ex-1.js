be5eb41ba4e27b35a2dd9420d0f43fbf6f156f70
nueng <wongsaokrn.kump@gmail.com>
2024-05-14 14:22:11 +0700

    feat: add answer code

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,22 @@
-// Start coding here
+function add(a,b){
+    return a+b
+}
+let addNum = add(10,5)
+
+
+function subtract(a,b){
+    return a-b
+}
+let addSub = subtract(10,5)
+
+
+function multiply(a,b){
+    return a*b
+}
+let result = multiply(10,5)
+
+
+function divide(a,b){
+    return a/b
+}
+let addDivide = divide(10,5)
--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,12 @@
-// Start coding here
+let add = (a,b) => {
+    return a+b
+}
+
+let subtract = (a,b) => {
+    return a-b
+}
+
+let multiply = (a,b) => a*b
+
+
+let divide = (a,b) => a/b
